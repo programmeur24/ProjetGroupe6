410f4f96d181079940bd97457e3489e6ce37f1f3
christbryan <58194263+christbryan@users.noreply.github.com>
2020-05-21 14:17:37 +0100

    view part

---
+++ JPU-BlankProject/view/src/main/java/view/Keyboard.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Keyboard implements KeyListener
+ {
+	
+	public void keyPressed(KeyEvent e) {
+	}
+	public void keyReleased(KeyEvent e) {
+	}
+	public void keyTyped(KeyEvent arg0) {
+		
+	}
+}
