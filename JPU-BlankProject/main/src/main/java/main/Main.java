d6730f411d91829ad036c261e5079c13fc89a0fc
programmeur24 <58194263+programmeur24@users.noreply.github.com>
2020-05-26 06:43:01 +0100

    suite...

--- JPU-BlankProject/main/src/main/java/main/Main.java
+++ JPU-BlankProject/main/src/main/java/main/Main.java
@@ -8,7 +8,7 @@ import view.View;
 public abstract class Main {
 
     public static void main(final String[] args) {
-    	
+    	//***
         final Model model = new Model();   
         final View view = new View(model);
         final Controller controller = new Controller(view, model);
