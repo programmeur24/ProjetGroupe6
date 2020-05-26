59afe3160a16b345df51ee6ae4064231837b5884
programmeur24 <58194263+programmeur24@users.noreply.github.com>
2020-05-26 06:31:24 +0100

    SUITE

--- JPU-BlankProject/main/src/main/java/main/Main.java
+++ JPU-BlankProject/main/src/main/java/main/Main.java
@@ -8,12 +8,12 @@ import view.View;
 public abstract class Main {
 
     public static void main(final String[] args) {
-    	//****Declaration****//
+    	
         final Model model = new Model();   
         final View view = new View(model);
         final Controller controller = new Controller(view, model);
         
-        
+      //****Declaration****//
         view.setController(controller);
         controller.control();
         controller.orderPerform(ControllerOrder.Map1); 
